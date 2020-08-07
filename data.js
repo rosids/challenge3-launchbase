136027876f0366a3ecd093f3fe49e126b114ce81
Rosiele Serpa <rosieleserpa@hotmail.com>
2020-08-07 11:59:15 -0300

    feat: add data

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "starter",
+        description: '<strong class= "font-color starter">Torne-se um programador desejado</strong> <br> no mercado com esses cursos <br> gratuitos.',
+        numberOfModules: "5 módulos",
+        price: "Gratuito"
+    },
+    {
+        id: "launchbase",
+        description: '<strong class="font-color launchbase"> Domine programação do zero </strong> e tenha acesso às melhores oportunidades do mercado.',
+        numberOfModules: "8 módulos",
+        price: "Pago"
+    },
+    {
+        id: "gostack",
+        description: '<strong class= "font-color gostack">Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile.',
+        numberOfModules: "15 módulos",
+        price: "Pago"
+    }
+]
